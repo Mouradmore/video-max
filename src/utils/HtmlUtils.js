09ed9b9e237acbf9aeca2fe9be45a41c186ca0a5
Mouradmore <mouradx524@gmail.com>
2024-10-22 19:11:57 +0100

    Update HtmlUtils.js

--- src/utils/HtmlUtils.js
+++ src/utils/HtmlUtils.js
@@ -8,7 +8,7 @@ import linkifyHtml from "linkify-html";
 
 export const rewriteDescription = text => {
     return linkifyHtml(text)
-        .replaceAll(/(?:http(?:s)?:\/\/)?(?:www\.)?youtube\.com(\/[/a-zA-Z0-9_?=&-]*)/gm, "$1")
-        .replaceAll(/(?:http(?:s)?:\/\/)?(?:www\.)?youtu\.be\/(?:watch\?v=)?([/a-zA-Z0-9_?=&-]*)/gm, "/watch?v=$1")
+        .replaceAll(/(?:http(?:s)?:\/\/)?(?:www\.)? TikTok\.com(\/[/a-zA-Z0-9_?=&-]*)/gm, "$1")
+        .replaceAll(/(?:http(?:s)?:\/\/)?(?:www\.)? TikT\.ok\/(?:watch\?v=)?([/a-zA-Z0-9_?=&-]*)/gm, "/watch?v=$1")
         .replaceAll("\n", "<br>");
 };
